604680835e0b91071db7d7ac884b5ab39d9b7c5b
kinelit <kinelit@mail.ru>
2016-06-28 11:33:23 +0400

    3 DrawerLayout

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,25 +1,42 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity  implements View.OnClickListener{
 
     private static final String TAG= ConstantManager.TAG_PREFIX+"Main Activity";
+    private ImageView mCallImg;
+    private CoordinatorLayout mCoordinatorLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+        mCallImg = (ImageView)findViewById(R.id.call_img);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
+        mCallImg.setOnClickListener(this);
+
+
         if (savedInstanceState == null) {
+            showSnackbar("активити запускается впервые");
+            showToast("активити запускается");
 
         } else {
-
+            showSnackbar("активити уже создавалось");
+            showToast("активити создавалось");
         }
     }
 
@@ -57,5 +74,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "onRestart");
+        }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.call_img:
+               /* showProgress();
+               runWithDelay();*/
+
+                break;
+        }
     }
+    @Override
+protected void  onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+}
+    private void showSnackbar(String message ){
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG ).show();
+    }
+    /*private void runWithDelay(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hideProgress();
+            }
+        }, 5000);
+    }*/
 }
